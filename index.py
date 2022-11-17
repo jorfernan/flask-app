78d70e76bb5b202b239899fec588861f84748c97
jorfernan <jorfernan1995@gmail.com>
2022-11-17 11:24:51 +0100

    Connection functon created, implentation inside index function and all posts from DB queried

--- index.py
+++ index.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
+import sqlite3
+
+# Database File
+DB_FILE = 'database.db'
 
 app = Flask(__name__)
 
+""" Function that establish a connection to the database file"""
+def get_db_connection():
+    conn = sqlite3.connect(DB_FILE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
 @app.route('/')
 def index():
+    # Get all posts from database file
+    conn = get_db_connection() # Creating conection
+    posts = conn.execute('SELECT * FROM posts').fetchall() # Executin query
+    conn.close() # Closing connection
     return render_template("index.html")
