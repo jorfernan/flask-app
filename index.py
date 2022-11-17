6b3661896f7f78d46bbf2abe32038dc9a9b20795
jorfernan <jorfernan1995@gmail.com>
2022-11-17 11:36:40 +0100

    Function that returns an specific post from the DB based on its id number, and returns a 404 if there is no match

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
 import sqlite3
+from werkzeug.exceptions import abort
 
 # Database File
 DB_FILE = 'database.db'
@@ -13,6 +14,16 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+"""Function that gets an specific post taking its id as an argument"""
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',(post_id,)).fetchone()
+    conn.close()
+    # Checking for results, if not returns a 404
+    if post is None:
+        abort(404)
+    return post
+    
 
 @app.route('/')
 def index():
