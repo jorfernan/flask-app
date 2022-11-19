f28059e6567c6557e7f6af1b6b69db7b525ae3f9
jorfernan <jorfernan1995@gmail.com>
2022-11-19 10:56:34 +0100

    Semicolons added to insert statements inside schema.sql

--- schema.sql
+++ schema.sql
@@ -7,8 +7,8 @@ CREATE TABLE posts (
     content TEXT NOT NULL
 );
 
-INSERT INTO posts (title, content) VALUES ('First Post','Content for the first post')
-INSERT INTO posts (title, content) VALUES ('Second Post','Content for the second post')
-INSERT INTO posts (title, content) VALUES ('Third Post','Content for the third post')
-INSERT INTO posts (title, content) VALUES ('Fourth Post','Content for the fourth post')
-INSERT INTO posts (title, content) VALUES ('Fith Post','Content for the fith post')
+INSERT INTO posts (title, content) VALUES ('First Post','Content for the first post');
+INSERT INTO posts (title, content) VALUES ('Second Post','Content for the second post');
+INSERT INTO posts (title, content) VALUES ('Third Post','Content for the third post');
+INSERT INTO posts (title, content) VALUES ('Fourth Post','Content for the fourth post');
+INSERT INTO posts (title, content) VALUES ('Fith Post','Content for the fith post');
